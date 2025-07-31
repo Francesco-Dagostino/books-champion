1ab87c006cc1114c6612571a743ce928351ec6b5
Francesco-Dagostino <francescodagostino1889@gmail.com>
2025-07-30 21:04:38 -0300

    Clase 1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import BookItem from './components/bookItem/BookItem'
+
+function App() {
+  
+  return (
+    <>
+      <h2>Books Champion App"</h2>
+      <p>¡Quiero leer libros!</p>
+      <BookItem />
+    </>
+  )
+}
+
+export default App
