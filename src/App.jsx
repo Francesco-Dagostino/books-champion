3710a8ae7dfd4153713c75c4fb5c0db573c0a679
Francesco-Dagostino <francescodagostino1889@gmail.com>
2025-08-20 20:26:20 -0300

    Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,21 @@
-import { useState } from 'react';
+import { BrowserRouter, Route, Routes } from "react-router"
+import Dashboard from "./components/Dashboard/Dashboard"
+import Login from "./auth/login/Login"
+import { Book } from "react-bootstrap-icons"
 import './App.css'
-import Books from './components/books/Books';
-import BookItem from './components/bookItem/BookItem'
-import NewBook from './components/newBook/NewBook';
-import BooksSearch from './components/bookSearch/BooksSearch';
-
-const booksInitial = [
-  {
-    title: "100 años de soledad",
-    author: "Gabriel García Marquez",
-    rating: 5,
-    pageCount: 410,
-    imageUrl:
-      "https://images.cdn3.buscalibre.com/fit-in/360x360/61/8d/618d227e8967274cd9589a549adff52d.jpg",
-    available: true,
-  },
-  {
-    title: "Asesinato en el Orient Express",
-    author: "Agatha Christie",
-    rating: 4,
-    pageCount: 256,
-    imageUrl:
-      "https://m.media-amazon.com/images/I/71RFyM95qwL._AC_UF1000,1000_QL80_.jpg",
-    available: true,
-  },
-  {
-    title: "Las dos torres",
-    author: "J.R.R Tolkien",
-    rating: 5,
-    pageCount: 352,
-    imageUrl:
-      "https://m.media-amazon.com/images/I/A1y0jd28riL._AC_UF1000,1000_QL80_.jpg",
-    available: true,
-  },
-  {
-    title: "50 sombras de Grey",
-    author: "E.L James",
-    rating: 1,
-    pageCount: 514,
-    imageUrl:
-      "https://prodimage.images-bn.com/pimages/9781728260839_p0_v2_s1200x630.jpg",
-    available: true,
-  },
-];
+import NotFound from "./components/notFound/NotFound"
 
 function App() {
-
-  const[bookList, setBookList] = useState(booksInitial)
-
-  const handleBookAdded = (enteredBook) =>
-  {
-    const bookData = {
-      ...enteredBook,
-      id: Math.random()
-    }
-    setBookList(prevBooksList => [bookData, ...prevBooksList])
-
-  } 
-
   
   return (
-    <div className='d-flex flex-column aling-items-center'>
-      <h2>Books Champion App"</h2>
-      <p>¡Quiero leer libros!</p>
-      <NewBook onBookAdded={handleBookAdded}/>
-      <Books books={bookList}/>
-
-      
+    <div className='d-flex flex-column align-items-lg-center'>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/dashboard" element={<Dashboard/>}/>
+          <Route path="/*" element={<NotFound/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
